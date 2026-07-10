be7f6b7e43282b0fc14154a806954bb807247171
Сказка <300078240+imcka3ka@users.noreply.github.com>
2026-07-10 17:02:14 +0200

    blueprint of the game

--- main.c
+++ main.c
@@ -7,6 +7,19 @@ int main(void){
     char hidden_word[len + 1];
     memset(hidden_word, '_', len); 
     hidden_word[len] = '\0';
-    printf("%s\n%s\n", word_to_guess, hidden_word);
+
+
+    char input = ' ';
+    while(strcmp(word_to_guess, hidden_word)){
+        printf("Type a letter: ");
+        input = getchar();
+
+        for(int i = 0; i < len; i++){
+            if(word_to_guess[i] == input){
+                hidden_word[i] = input;
+            }
+        }
+    }
+    printf("\n\n%s\n%s\n", word_to_guess, hidden_word);
     return 0;
 }
